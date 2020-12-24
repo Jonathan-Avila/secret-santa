43ce91dcac6d886063cdd99602308e2838dd1d33
Jonathan <jonathan.avila@nyu.edu>
2020-12-23 22:38:53 -0500

    typo fix

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # secret-santa
-A python script that randomly determines the order of gift giving from a secret santa draw and email the participants their independent result/gift receivers info. 
-It is designed to not allow anybody to know who anybody else is giving a gift to. The minimum number of participants must be 3 and the maximum is 10,000. You must add the participants' information into the config file.
+A python script that randomly determines the order of gift giving from a secret santa draw and emails the participants their independent result/gift receivers info. It is designed to not allow anybody to know who anybody else is giving a gift to. The minimum number of participants must be 3 and the maximum is 10,000. You must add the participants' information into the config file.
--- secret_santa_config.py
+++ secret_santa_config.py
@@ -1,6 +1,11 @@
+#names_of_participants = ["Anna", "Beth", "Charlie"] # Enter the names of all the paricipants here as comma separated strings in a list i.e. ["Anna", "Beth", "Charlie"]
+#personal_info_dict = {"Anna": ("jonathanavila900@gmail.com", "1 Central Park Ave, Penthouse Suite 1"), "Beth": ("jonathanavila900@gmail.com", "2 Central Park Ave, Penthouse Suite 1"), "Charlie": ("jonathanavila900@gmail.com", "3 Central Park Ave, Penthouse Suite 1")} # Enter the info of all the paricipants here as a dict with names as keys and a tuple containing an email address and physical address as the value i.e. {"Anna":(anna@email.com, "111 Main st") "Beth":(beth@email.com, "222 Main st") "Charlie":(charlie@email.com, "333 Main st")}
 
-names_of_participants = ["Anna", "Beth", "Charlie"] # Enter the names of all the paricipants here as comma separated strings in a list i.e. ["Anna", "Beth", "Charlie"]
-personal_info_dict = {"Anna": ("anna@email.com", "1 Central Park Ave, Penthouse Suite 1"), "Beth": ("beth@email.com", "2 Central Park Ave, Penthouse Suite 1"), "Charlie": ("charlie@email.com", "3 Central Park Ave, Penthouse Suite 1")} # Enter the info of all the paricipants here as a dict with names as keys and a tuple containing an email address and physical address as the value i.e. {"Anna":(anna@email.com, "111 Main st") "Beth":(beth@email.com, "222 Main st") "Charlie":(charlie@email.com, "333 Main st")}
+
+names_of_participants = ["Brian", "Nicholas", "Sarah", "Nikky", "Jonathan", "Emanuel"] # Enter the names of all the paricipants here as comma separated strings in a list i.e. ["Anna", "Beth", "Charlie"]
+
+personal_info_dict = {"Brian":("brianjamescouch@gmail.com","616 East Lincoln Ave, Apt C53, Mount Vernon, NY 10552"), "Sarah":("scl475@nyu.edu","110-14 Astoria Blvd. Apt. 6F East Elmhurst, NY 11369"), "Emanuel":("ebazov@gmail.com","415 Albemarle Road apt. 6D Brooklyn NY, 11218"), "Jonathan":("jonathanavila900@gmail.com","903 Rogers Pl Apt 23 Bronx, NY 10459"), "Nicholas":("njlmarshall@gmail.com","442 halsey st, brooklyn, ny 11233"), "Nikky":("Nikoleagnitti@gmail.com","3410 Beverly Rd, apt 1. Brooklyn, NY, 11203")} # Enter the info of all the paricipants here as a dict with names as keys and a tuple containing an email address and physical address as the value i.e. {"Anna":(anna@email.com, "111 Main st") "Beth":(beth@email.com, "222 Main st") "Charlie":(charlie@email.com, "333 Main st")}
+
 
 def get_names_of_participants():
 
